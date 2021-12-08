5982a263009f587dd28565ff79af6e42da8f5681
fettytech <evansosab@gmail.com>
2021-12-08 19:39:49 +0000

    install some packages

--- Firestation_api/settings.py
+++ Firestation_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&ux6&r=ecd(5t)=_6yej99hi9=%1za1xt1vf*9ox#(wy&%6dh#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1/', 'firestation-api.herokuapp.com/']
 
